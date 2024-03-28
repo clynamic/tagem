19b4f9359d79b82ca0901fb041ddca7cfbc09712
clragon <clragon@clynamic.net>
2024-03-28 14:29:30 +0100

    updated cors logic

--- src/main/kotlin/net/clynamic/common/Cors.kt
+++ src/main/kotlin/net/clynamic/common/Cors.kt
@@ -1,5 +1,7 @@
 package net.clynamic.common
 
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.cors.routing.CORS
@@ -9,14 +11,38 @@ fun Application.configureCors() {
     val logger = LoggerFactory.getLogger("Cors")
 
     val hostUrl = attributes[ENVIRONMENT_KEY].get("HOST_URL", null)
+    val hostUrls = attributes[ENVIRONMENT_KEY].get("HOST_URLS", null)
 
     install(CORS) {
-        hostUrl?.let {
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Head)
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Patch)
+        allowMethod(HttpMethod.Delete)
+
+        allowHeader(HttpHeaders.ContentType)
+        allowHeader(HttpHeaders.Authorization)
+        allowCredentials = true
+
+        if (hostUrl != null) {
             logger.info("Allowing requests from $hostUrl")
             allowHost(hostUrl, listOf("http", "https"))
         }
+        hostUrls?.split(",")?.map { it.trim() }?.forEach { hostUrl ->
+            if (hostUrl.isNotEmpty()) {
+                logger.info("Allowing requests from $hostUrl")
+                allowHost(hostUrl, listOf("http", "https"))
+            }
+        }
+
         allowOrigins {
-            it.matches(Regex("^(https?://)?(localhost|127\\.\\d+\\.\\d+\\.\\d+)(:\\d+)?(/.*)?$"))
+            arrayOf(
+                it.matches(Regex("^(https?://)?(localhost)(:\\d+)?(/.*)?$")),
+                it.matches(Regex("^(https?://)?(127\\.\\d+\\.\\d+\\.\\d+)(:\\d+)?(/.*)?$")),
+                it.matches(Regex("^(https?://)?(0:0:0:0:0:0:0:1)(:\\d+)?(/.*)?$")),
+            ).any()
         }
         allowSameOrigin = true
     }
